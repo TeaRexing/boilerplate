cfa7992c81a55a2c68e8aeb65173c48f63809629
Patrick Zempel <info@patrickzempel.de>
2026-02-02 16:51:22 +0100

    added fontsource

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,49 @@
+const path = require("path");
+
 module.exports = {
-  entry: './src/js/main.js',
+  entry: "./src/js/main.js",
+
   output: {
-    path: __dirname + '/public_html',
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, "public_html"),
+    filename: "bundle.js",
   },
+
   resolve: {
-    symlinks: true
+    // verhindert unnötige "browser"-Alias-Warnungen
+    mainFields: ["module", "main"],
+    symlinks: true,
   },
+
   module: {
-    rules: [{
-      test: /\.m?js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env']
-        }
-      }
-    }]
-  }
-}
+    rules: [
+      // JavaScript / Babel
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+
+      // CSS (wichtig für @font-face von Fontsource)
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+
+      // Fonts (Webpack 5 Asset Modules)
+      {
+        test: /\.(woff2?|eot|ttf|otf)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "fonts/[name][ext]",
+        },
+      },
+    ],
+  },
+};
+
+
