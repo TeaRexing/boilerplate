08fbd5df2894aaf4267f33be5e129e85eee0771e
Patrick Zempel <info@patrickzempel.de>
2026-08-26 10:12:20 +0200

    updated and refactored build pipeline

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,37 @@
-const path = require("path");
+let webpack = require('webpack');
 
 module.exports = {
-  entry: "./src/js/main.js",
-
+  entry: './src/js/main.js',
   output: {
-    path: path.resolve(__dirname, "public_html"),
-    filename: "bundle.js",
+    path: __dirname + '/public_html',
+    filename: 'bundle.js',
+    library: 'pzLib',
   },
-
   resolve: {
-    // verhindert unnötige "browser"-Alias-Warnungen
-    mainFields: ["module", "main"],
-    symlinks: true,
+    symlinks: true
   },
-
   module: {
     rules: [
-      // JavaScript / Babel
       {
         test: /\.m?js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ["@babel/preset-env"],
-          },
+            presets: ['@babel/preset-env']
+          }
         },
       },
-
-      // CSS (wichtig für @font-face von Fontsource)
       {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-
-      // Fonts (Webpack 5 Asset Modules)
-      {
-        test: /\.(woff2?|eot|ttf|otf)$/i,
-        type: "asset/resource",
-        generator: {
-          filename: "fonts/[name][ext]",
-        },
-      },
-    ],
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
   },
-};
-
-
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery"
+    })
+  ]
+}
